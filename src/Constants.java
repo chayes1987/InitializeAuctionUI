c702e5e1859fe7ed233c0b0ba9b0c2f840606bc6
Conor Hayes <ch1987@live.ie>
2015-02-01 17:24:36 +0000

    Added constants class. Added library for zmq integration. Added code to subscribe for auction running event and created a publisher for the acknowledgement.

---
+++ src/Constants.java
@@ -0,0 +1,9 @@
+/*
+    @author Conor Hayes
+ */
+
+public class Constants {
+    public static final String PUB_ADR = "tcp://127.0.0.1:2000";
+    public static final String SUB_ADR = "tcp://127.0.0.1:1111";
+    public static final String TOPIC = "AuctionRunning";
+}
--- src/InitializeAuctionUI.java
+++ src/InitializeAuctionUI.java
@@ -1,9 +1,24 @@
+import org.jeromq.ZMQ;
 
 /*
     @author Conor Hayes
  */
 public class InitializeAuctionUI {
-    public static void main(String[] args) {
+    private ZMQ.Context context = ZMQ.context();
+    private ZMQ.Socket publisher = context.socket(ZMQ.PUB);
 
+    public static void main(String[] args) { new InitializeAuctionUI().subscribe(); }
+
+    private void subscribe(){
+        ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
+        subscriber.connect(Constants.SUB_ADR);
+        subscriber.subscribe(Constants.TOPIC.getBytes());
+        System.out.println("SUB: " + Constants.TOPIC);
+        publisher.bind(Constants.PUB_ADR);
+
+        while(true){
+            String auctionRunningEvt = new String(subscriber.recv());
+            System.out.println("REC: " + auctionRunningEvt);
+        }
     }
 }
