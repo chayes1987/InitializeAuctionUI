656447b92b60aa44f34909cbe9fa466596de806f
Conor Hayes <ch1987@live.ie>
2015-04-01 23:05:12 +0100

    Formatting

--- src/test/java/TestInitializeAuctionUI.java
+++ src/test/java/TestInitializeAuctionUI.java
@@ -7,13 +7,17 @@ import java.lang.reflect.Method;
 public class TestInitializeAuctionUI {
     
     @Test
-    public void testParseMessage() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void testParseMessage() throws NoSuchMethodException, InvocationTargetException,
+            IllegalAccessException {
         InitializeAuctionUI ui = new InitializeAuctionUI();
-        Method parseMessage = InitializeAuctionUI.class.getDeclaredMethod("parseMessage", String.class, String.class, String.class);
+        Method parseMessage = InitializeAuctionUI.class.getDeclaredMethod("parseMessage",
+                String.class, String.class, String.class);
         parseMessage.setAccessible(true);
-        String returnValue = (String) parseMessage.invoke(ui, "StartAuction <id>1</id>", "<id>", "</id>");
+        String returnValue = (String) parseMessage.invoke(ui, "StartAuction <id>1</id>",
+                "<id>", "</id>");
         Assert.assertEquals("1", returnValue);
-        returnValue = (String) parseMessage.invoke(ui, "BidPlaced <params>2000</params>", "<params>", "</params>");
+        returnValue = (String) parseMessage.invoke(ui, "BidPlaced <params>2000</params>",
+                "<params>", "</params>");
         Assert.assertEquals("2000", returnValue);
     }
 }
