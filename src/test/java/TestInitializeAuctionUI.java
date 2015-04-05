84007533245f8cf118d53ce60f45e3492abe1407
Conor Hayes <ch1987@live.ie>
2015-04-05 17:50:19 +0100

    Added Test

--- src/test/java/TestInitializeAuctionUI.java
+++ src/test/java/TestInitializeAuctionUI.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Properties;
 
 public class TestInitializeAuctionUI {
     
@@ -19,5 +20,16 @@ public class TestInitializeAuctionUI {
         returnValue = (String) parseMessage.invoke(ui, "BidPlaced <params>2000</params>",
                 "<params>", "</params>");
         Assert.assertEquals("2000", returnValue);
+    }
+
+    @Test
+    public void testReadConfig() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        InitializeAuctionUI ui = new InitializeAuctionUI();
+        Method readConfig = InitializeAuctionUI.class.getDeclaredMethod("readConfig");
+        readConfig.setAccessible(true);
+        Properties returnValue = (Properties) readConfig.invoke(ui);
+        Assert.assertNotEquals(null, returnValue);
+        Assert.assertEquals("InitializeAuctionUI", returnValue.getProperty("SERVICE_NAME"));
+        Assert.assertEquals("AuctionRunning", returnValue.getProperty("TOPIC"));
     }
 }
